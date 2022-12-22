9dcb908d211f8112e6827bdc9725694815d21a08
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2022-12-22 14:20:03 +0545

    odwoodwo

---
+++ components/models/Createspacemodal.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { useSession } from "next-auth/react";
+import Modal from "react-modal";
+
+const Createspacemodal = ({ isOpen, onRequestClose }: any) => {
+  const [name, setName] = useState("");
+
+  const session = useSession();
+  console.log(session)
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+      width: "400px",
+      height: "190px",
+    },
+  };
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault();
+    try {
+      const response = await fetch("api/spaces/createspace", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(name),
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      style={customStyles}
+      ariaHideApp={false}
+    >
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2">
+            Name:
+          </label>
+          <input
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline form-input"
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            type="submit"
+            className="bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-r text-white font-bold py-2 px-4 rounded w-full focus:outline-none focus:shadow-outline"
+          >
+            Create
+          </button>
+        </div>
+      </form>
+    </Modal>
+  );
+};
+
+export default Createspacemodal;
