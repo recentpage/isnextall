58e5e759e5f2f46316f737c89e0b3b03685b8cc7
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2022-12-23 17:21:10 +0545

    owdw

--- pages/api/getcopy/index.ts
+++ pages/api/getcopy/index.ts
@@ -4,7 +4,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 const openai = async (req: NextApiRequest, res: NextApiResponse) => {
   const { productname, productcharacteristics } = req.body;
   const configuration = new Configuration({
-    apiKey: "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",
+    apiKey: "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",
   });
   const api = new OpenAIApi(configuration);
   const prompt = `Generate a product description for a product with the following attributes: product name = '${productname}', product description = '${productcharacteristics}'. Make sure to include details about the product's features and benefits. genrate 3 variations of the product description.`;
